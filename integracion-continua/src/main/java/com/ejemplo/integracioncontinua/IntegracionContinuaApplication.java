d9c26b84f5fc751fd668fcbd216213f8edf2cba9
msemarti <44371802+msemarti@users.noreply.github.com>
2018-10-23 16:12:14 +0200

    Add files via upload

---
+++ integracion-continua/src/main/java/com/ejemplo/integracioncontinua/IntegracionContinuaApplication.java
@@ -0,0 +1,12 @@
+package com.ejemplo.integracioncontinua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracionContinuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracionContinuaApplication.class, args);
+	}
+}
